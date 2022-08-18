3032ce29d0e2ec69a2b961e43e32adcdcfbca39d
nadia.trfm <69869414+Nadia-ru@users.noreply.github.com>
2022-08-18 19:05:20 +0300

    first commit

---
+++ src/main/java/org/example/Start.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.example.telegram.Bot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Start {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+}
+}
